a35c31d4ed84f7ff3e2664ab9c344a535929beaa
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-05-10 14:33:23 +0200

    ADD: movie and TVserie constructors

--- models/TVserie.php
+++ models/TVserie.php
@@ -6,11 +6,18 @@
 class TVserie extends Production{
 
     public $seasons;
+
+    function __construct($_img,$_title,$_language,$_vote,$_genre,$_seasons = 0) 
+    {
+        parent::__construct($_img,$_title,$_language,$_vote,$_genre);
+        $this->seasons = $_seasons;
+        
+    }
 } 
 
 $the_witcher = new TVserie('https://support.mondadoristore.it/in/speciali/fantasticstore/witcher/locandina3.gif','The Witcher','EN','10',$action);
 
-$the_witcher->seasons = 3;
+// $the_witcher->seasons = 3;
 
 
 ?>
--- models/movie.php
+++ models/movie.php
@@ -7,14 +7,23 @@ class Movie extends Production{
 
     public $profits;
     public $duration;
+
+    function __construct($_img,$_title,$_language,$_vote,$_genre,$_profits,$_duration) 
+    {
+        parent::__construct($_img,$_title,$_language,$_vote,$_genre);
+        $this->profits = $_profits;
+        $this->duration = $_duration;
+    }
+
+    
 } 
 
-$harry_potter = new Movie('https://static.posters.cz/image/1300/poster/harry-potter-la-pietra-filosofale-i104639.jpg','Harry Potter','IT','8',$fantasy);
-$spiderman = new Movie('https://m.media-amazon.com/images/I/6193yNvh1RL._AC_UF894,1000_QL80_.jpg','Spiderman','IT','7',$animated);
+$harry_potter = new Movie('https://static.posters.cz/image/1300/poster/harry-potter-la-pietra-filosofale-i104639.jpg','Harry Potter','IT','8',$fantasy,'millemila €','120 min');
+$spiderman = new Movie('https://m.media-amazon.com/images/I/6193yNvh1RL._AC_UF894,1000_QL80_.jpg','Spiderman','IT','7',$animated,'millemila €','180 min');
 
-$spiderman->profits = 'millemila €';
-$spiderman->duration = '120 min';
-$harry_potter->profits = 'millemila €';
-$harry_potter->duration = '120 min';
+// $spiderman->profits = 'millemila €';
+// $spiderman->duration = '120 min';
+// $harry_potter->profits = 'millemila €';
+// $harry_potter->duration = '120 min';
 
 ?>
